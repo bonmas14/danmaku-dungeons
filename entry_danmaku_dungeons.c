4e9b6c330c934974cb5eb9aab225459ab61b762a
bonmas14 <bonmas14@gmail.com>
2024-07-20 19:44:39 +0300

    health and shooting

--- entry_danmaku_dungeons.c
+++ entry_danmaku_dungeons.c
@@ -1,15 +1,18 @@
 #define MAX_ENTITY_COUNT 4096
 #define CAMERA_SCALE 5.0f
 #define CAMERA_SPEED 1.0f
-#define PLAYER_SPEED 2.5f
+#define PLAYER_SPEED 4.5f
+#define PLAYER_BULLET_SPEED 20.0f
 #define BULLET_01_SPEED 10.0f
 #define SHOOT_INTERVAL 0.1f
-#define PLAYER_HEALTH 100.0f
-#define ENEMY_HEALTH 20.0f
+#define PLAYER_HEALTHS 3
+#define ENEMY_HEALTHS 1 // bosses are like 100 - 1000
+#define BULLET_DAMAGE 1
 
-typedef enum entity_archetype_t {
+typedef enum entity_archetype_t { // do we need separate archetype for bullets? so we dont need swith (ent.entity_type) ??
     ENTITY_nil,
     ENTITY_enemy,
+    ENTITY_player_bullet, 
     ENTITY_bullet_01, 
     ENTITY_bullet_02, 
     ENTITY_player, 
@@ -42,9 +45,8 @@ typedef struct entity_t {
     Vector2 position;
     Vector2 direction;
 
-    bool shoot_by_player;
     float32 radius;
-    float32 health;
+    int32_t healths;
 
     float64 timer; // gonna need this for bullets
 
@@ -79,10 +81,18 @@ void entity_setup_player(entity_t* entity) {
     entity->is_collidable = true;
     entity->movement_speed = PLAYER_SPEED;
     entity->sprite = sprite_get(SPRITE_player);
-    entity->health = PLAYER_HEALTH;
-    entity->radius = entity->sprite.size.x / 2;
+    entity->healths = PLAYER_HEALTHS;
+    entity->radius = entity->sprite.size.x / 2 / 10; // THIS IS DANMAKU GAME, player collider should be small!
 }
 
+void entity_setup_player_bullet(entity_t* entity) {
+    entity->entity_type = ENTITY_player_bullet;
+    entity->is_visible = true;
+    entity->is_collidable = true;
+    entity->movement_speed = PLAYER_BULLET_SPEED;
+    entity->sprite = sprite_get(SPRITE_bullet_01);
+    entity->radius = entity->sprite.size.x / 2;
+}
 void entity_setup_bullet_01(entity_t* entity) {
     entity->entity_type = ENTITY_bullet_01;
     entity->is_visible = true;
@@ -122,8 +132,20 @@ entity_t* entity_create(void) {
     return 0;
 }
 
-void entity_destroy(entity_t* ent) {
-    ent->is_valid = false;
+void entity_clear_all_enemy_bullets(void) {
+    for (u32 i = 0; i < MAX_ENTITY_COUNT; i++) {
+        entity_t* bullet = &(world->entities[i]);
+
+        if (!bullet->is_valid) continue;
+
+        switch (bullet->entity_type) {
+            case ENTITY_bullet_01:
+                bullet->is_valid = false;
+                break;
+            default:
+            break;
+        }
+    }
 }
 
 void update_player(void) {
@@ -139,7 +161,9 @@ void update_player(void) {
         input_axis.x += 1.0f;
 
     input_axis = v2_normalize(input_axis);
+
     input_axis = v2_mulf(input_axis, player->movement_speed * delta_time);
+    
     player->position = v2_add(player->position, input_axis);
 
     if (!is_key_down(KEY_SPACEBAR))
@@ -148,19 +172,25 @@ void update_player(void) {
     if ((player->timer + SHOOT_INTERVAL) < now_time) {
         player->timer = now_time;
 
-        for (size_t i = 0; i <= 10; i++) {
+        size_t count = 3;
+
+        float32 shoot_angle = PI32 / 16.0;
+        for (size_t i = 0; i <= count; i++) {
             entity_t* ent = entity_create();
 
             if (ent == 0) break;
 
-            entity_setup_bullet_01(ent);
-            ent->shoot_by_player = true;
+            entity_setup_player_bullet(ent);
             ent->position = player->position;
-            ent->direction = v2_normalize(v2(get_random_float32_in_range(-1, 1), get_random_float32_in_range(-1, 1)));
-            //float64 y = sin((float64)i / 100.0 * M_PI);
-            //float64 x = cos((float64)i / 100.0 * M_PI);
 
-            //ent->direction = v2(x, y);
+            float32 normalized = (float32)i / (float32)count;
+
+            float32 angle = normalized * shoot_angle + PI32 / 2 - (shoot_angle / 2);
+
+            float32 y = sinf(angle);
+            float32 x = cosf(angle);
+
+            ent->direction = v2(x, y);
         }
     }
 }
@@ -174,12 +204,14 @@ void update_enemies(void) {
 
         if (enemy.entity_type != ENTITY_enemy) continue;
 
-        if ((enemy.timer + SHOOT_INTERVAL) > now_time)
+        if ((enemy.timer + SHOOT_INTERVAL * 2.0) > now_time)
             continue;
 
         enemy.timer = now_time;
 
-        for (size_t i = 0; i <= 10; i++) {
+        size_t count = 10;
+
+        for (size_t i = 0; i <= count; i++) {
             entity_t* bullet = entity_create();
             if (bullet == 0) break;
 
@@ -187,8 +219,10 @@ void update_enemies(void) {
             bullet->position = enemy.position;
             //bullet->direction = v2_normalize(v2(get_random_float64_in_range(-1, 1), get_random_float64_in_range(-1, 1)));
 
-            float32 y = sinf(((float32)i / 10.0) * 2.0 * M_PI);
-            float32 x = cosf(((float32)i / 10.0) * 2.0 * M_PI);
+            float32 normalized = ((float32)i / (float32)count);
+
+            float32 y = sinf(normalized * 2.0 * PI32 + 1 * now_time);
+            float32 x = cosf(normalized * 2.0 * PI32 + 1 * now_time);
 
             bullet->direction = v2(x, y);
         }
@@ -199,33 +233,59 @@ void update_enemies(void) {
 
 void update_bullets(void) {
     for (size_t i = 0; i < MAX_ENTITY_COUNT; i++) {
-        entity_t bullet = world->entities[i];
+        entity_t* bullet = &(world->entities[i]);
 
-        if (!bullet.is_valid) continue;
+        if (!bullet->is_valid) continue;
 
-        switch (bullet.entity_type) { 
+        // each bullet has its unique movement pattern, but not right now as you see
+
+        switch (bullet->entity_type) { 
             case ENTITY_bullet_01:
-                if (bullet.timer > 1.0) {
-                    bullet.is_valid = false;
-                    break;
-                }
+                if (bullet->timer > 1.0) { // just for tests, it should break as soon as it go over screen
+                    bullet->is_valid = false;
+                    continue;
+                } 
 
-                bullet.position = v2_add(bullet.position, v2_mulf(bullet.direction, bullet.movement_speed * delta_time));
-                bullet.timer += delta_time;
+                bullet->position = v2_add(bullet->position, v2_mulf(bullet->direction, bullet->movement_speed * delta_time));
+                bullet->timer += delta_time;
+                break; 
+            case ENTITY_player_bullet:
+                if (bullet->timer > 1.0) { // just for tests, it should break as soon as it go over screen
+                    bullet->is_valid = false;
+                    continue;
+                } 
 
-                if (check_collision_circle_to_circle(circle(player->radius, player->position), circle(bullet.radius, bullet.position))) {
-                    log("collided %f %f", bullet.position.x, bullet.position.y);
-                }
+                bullet->position = v2_add(bullet->position, v2_mulf(bullet->direction, bullet->movement_speed * delta_time));
+                bullet->timer += delta_time;
                 break;
             default: break;
-        } 
+        }
 
-        world->entities[i] = bullet;
+        // collisions
+        switch (bullet->entity_type) { 
+            case ENTITY_bullet_01:
+                if (check_collision_circle_to_circle(circle(player->radius, player->position), circle(bullet->radius, bullet->position))) {
+                    player->healths -= BULLET_DAMAGE;
+                    bullet->is_valid = false; // play cool animation and delete all bullets instead
+    
+                    entity_clear_all_enemy_bullets();
+                    return;
+                }
+                break;
+            case ENTITY_player_bullet:
+                break;
+                // if we want attack enemies, we need to scan all enemies and bullets N^2
+                // that would be bad so we need to separate bullets from entities.
+                // it will make live easier
+                //
+                // if (check_collision_circle_to_circle(circle(player->radius, player->position), circle(bullet->radius, bullet->position))) { }
+            default: break;
+        } 
     }
 }
 
 int entry(int argc, char **argv) {
-	window.title = STR("Minimal Game Example");
+	window.title = STR("Danmaku dungeons");
 	window.scaled_width = 1280; // We need to set the scaled size if we want to handle system scaling (DPI)
 	window.scaled_height = 720; 
 	window.x = 200;
@@ -288,21 +348,29 @@ int entry(int argc, char **argv) {
             if (!ent.is_valid) continue;
             if (!ent.is_visible) continue;
 
+            Vector4 color = COLOR_WHITE;
+
+            Matrix4 xform = m4_scalar(1.0);
+            Vector2 offset = v2_mulf(ent.sprite.size, -0.5f);
+            xform = m4_translate(xform, v3(offset.x, offset.y, 0));
+            xform = m4_translate(xform, v3(ent.position.x, ent.position.y, 0));
+
             switch (ent.entity_type) {
+                case ENTITY_player_bullet:
+                    color = hex_to_rgba(0xFFE1BF7F);
+                    break;
                 default:
-                    Matrix4 xform = m4_scalar(1.0);
-                    Vector2 offset = v2_mulf(ent.sprite.size, -0.5f);
-                    xform = m4_translate(xform, v3(offset.x, offset.y, 0));
-                    xform = m4_translate(xform, v3(ent.position.x, ent.position.y, 0));
-                    draw_image_xform(ent.sprite.image, xform, ent.sprite.size, COLOR_WHITE);
+                    color = COLOR_WHITE;
                     break;
             }
+
+            draw_image_xform(ent.sprite.image, xform, ent.sprite.size, color);
         }
 
-        draw_frame.projection = m4_make_orthographic_projection(window.pixel_width * -0.5, window.pixel_width * 0.5, window.pixel_height * -0.5, window.pixel_height * 0.5, -1, 10);
+        draw_frame.projection = m4_make_orthographic_projection(0, window.pixel_width, -window.pixel_height, 0, -1, 10); // topleft
         draw_frame.view = m4_make_scale(v3(1, 1, 1));
 
-        draw_text(font, STR("Ligma"), 32, v2(0, 0), v2(1, 1), COLOR_WHITE);
+        draw_text(font, tprintf("healths: %d", player->healths), 48, v2(0, -24), v2(1, 1), COLOR_WHITE);
 
         os_update(); 
 		gfx_update();
--- utils/collisions.c
+++ utils/collisions.c
@@ -9,15 +9,22 @@ typedef struct circle_t {
 // ---- functions ---- //
 inline circle_t circle(float32 radius, Vector2 position) { return (circle_t){ .radius = radius, .position = position }; }
 
-float v2_magn(Vector2 a) {
+float32 v2_magn(Vector2 a) {
     return sqrt(a.x * a.x + a.y * a.y);
+}
+
+float32 v2_sqr_magn(Vector2 a) {
+    return (a.x * a.x + a.y * a.y);
 }
 
 float32 v2_dist(Vector2 a, Vector2 b) {
     return v2_magn(v2_sub(a, b));
 }
 
+float32 v2_sqr_dist(Vector2 a, Vector2 b) {
+    return v2_sqr_magn(v2_sub(a, b));
+}
+
 bool check_collision_circle_to_circle(circle_t a, circle_t b) {
-    float32 dist = v2_dist(a.position, b.position);
-    return dist < (a.radius + b.radius);
+    return v2_sqr_dist(a.position, b.position) < (a.radius * a.radius + b.radius * b.radius);
 }
